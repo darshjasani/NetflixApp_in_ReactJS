8c534e49bd9b11f5b4bb580f2edb0e80616ee6e6
Darshkumar Jasani <jasanidarsh3@gmail.com>
2023-02-02 16:12:51 -0800

    First Update

---
+++ src/Data.jsx
@@ -0,0 +1,86 @@
+const Data =[
+    {
+      imgsrc : "https://wallpapercave.com/uwp/uwp1879033.jpeg",
+      title : "A Netflix Original Series",
+      sname : "The 100",
+      link : ""
+    },
+    {
+      imgsrc : "https://wallpapercave.com/dwp2x/wp2732715.jpg",
+      title : "A Netflix Original Series",
+      sname : "Suits",
+      link : ""
+    },
+    {
+      imgsrc : "https://wallpapercave.com/dwp2x/wp5394621.jpg",
+      title : "A Netflix Original Series",
+      sname : "Avengers",
+      link : ""
+    },
+
+    {
+      imgsrc : "https://wallpapercave.com/dwp2x/wp10909282.jpg",
+      title :"A Netflix Original Series",
+      sname : "Games of Thrones",
+      link : ""
+    },
+    {
+        imgsrc : "https://wallpapercave.com/uwp/uwp1879033.jpeg",
+        title : "A Netflix Original Series",
+        sname : "The 100",
+        link : ""
+      },
+  
+      {
+        imgsrc : "https://wallpapercave.com/uwp/uwp1879033.jpeg",
+        title : "A Netflix Original Series",
+        sname : "The 100",
+        link : ""
+      },
+  
+      {
+        imgsrc : "https://wallpapercave.com/uwp/uwp1879033.jpeg",
+        title : "A Netflix Original Series",
+        sname : "The 100",
+        link : ""
+      },
+  
+      {
+        imgsrc : "https://wallpapercave.com/uwp/uwp1879033.jpeg",
+        title : "A Netflix Original Series",
+        sname : "The 100",
+        link : ""
+      },
+  
+      {
+        imgsrc : "https://wallpapercave.com/uwp/uwp1879033.jpeg",
+        title : "A Netflix Original Series",
+        sname : "The 100",
+        link : ""
+      },
+  
+      {
+        imgsrc : "https://wallpapercave.com/uwp/uwp1879033.jpeg",
+        title : "A Netflix Original Series",
+        sname : "The 100",
+        link : ""
+      },
+  
+      {
+        imgsrc : "https://wallpapercave.com/uwp/uwp1879033.jpeg",
+        title : "A Netflix Original Series",
+        sname : "The 100",
+        link : ""
+      },
+  
+      {
+        imgsrc : "https://wallpapercave.com/uwp/uwp1879033.jpeg",
+        title : "A Netflix Original Series",
+        sname : "The 100",
+        link : ""
+      },
+  
+                    
+]
+
+export default Data;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import Data from './Data';
+import './index.css';
+ReactDOM.render(
+  <>
+    <h1 className='heading_info'>Top 5 Netflix Series in Year 2022</h1>
+    {Data.map((val)=>{
+        return (
+            <App
+              imgsrc = {val.imgsrc}
+              title = {val.title}
+              sname = {val.sname}
+              link = {val.link}
+             />
+        )
+    })}
+  </>
+  ,document.getElementById('root'));
